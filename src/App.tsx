ae14957f8715040362f10d725bf8e18dd9511184
Hasaan Mehmood <hasaan.mehmood.9@gmail.com>
2026-01-07 02:52:33 +0800

    Final Changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello to Blue NGINX!
+         Hello to Purple NGINX!
         </p>
         <a
           className="App-link"
